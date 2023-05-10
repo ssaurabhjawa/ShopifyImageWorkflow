790b523ad6794e55a09c76789e5587bf5cd4d4a9
Saurabh Jawa <ssaurabhjawa@gmail.com>
2023-05-10 13:14:58 +0530

    segregate

--- segregate_images.py
+++ segregate_images.py
@@ -17,21 +17,22 @@ def segregate_images_by_aspect_ratio():
     os.makedirs(os.path.join(folder_path, "0.80"), exist_ok=True)
     os.makedirs(os.path.join(folder_path, "0.714285714"), exist_ok=True)
     os.makedirs(os.path.join(folder_path, "1"), exist_ok=True)
-    os.makedirs(os.path.join(folder_path, "1.5"), exist_ok=True)
     os.makedirs(os.path.join(folder_path, "1.333333333"), exist_ok=True)
     os.makedirs(os.path.join(folder_path, "1.25"), exist_ok=True)
     os.makedirs(os.path.join(folder_path, "1.4"), exist_ok=True)
+    os.makedirs(os.path.join(folder_path, "1.5"), exist_ok=True)
 
-    # Define the aspect ratio thresholds and corresponding folder names
-    aspect_ratio_folders = {
-        0.666666667: "0.666666667",
-        0.75: "0.75",
-        0.80: "0.80",
-        0.714285714: "0.714285714",
-        1: "1",
-        1.25: "1.25",
-        1.333333333: "1.333333333",
-        1.5: "1.5",
+
+
+
+    # Define aspect ratio ranges and create folders
+    aspect_ratios = {
+        (0.0, 0.9): '0.8',
+        (0.9, 1.1): '1.0',
+        (1.1, 1.4): '1.3',
+        (1.4, 1.6): '1.5',
+        (1.6, 1.8): '1.8',
+        (1.8, float('inf')): '2.0'
     }
 
     for file_name in os.listdir(folder_path):
@@ -46,10 +47,17 @@ def segregate_images_by_aspect_ratio():
                 print(f"{file_name} is being used by another process, skipping...")
                 continue
 
-            # Use shutil.move to move the file to the respective aspect ratio folder
-            for threshold in sorted(aspect_ratio_folders.keys()):
-                if aspect_ratio <= threshold:
-                    new_file_path = os.path.join(folder_path, aspect_ratio_folders[threshold], file_name)
-                    shutil.move(file_path, new_file_path)
-                    break
+            # Use aspect_ratios dictionary to determine the appropriate folder
+            for range_, folder in aspect_ratios.items():
+                if range_[0] <= aspect_ratio < range_[1]:
+                    new_folder_path = os.path.join(folder_path, folder)
+                    if not os.path.exists(new_folder_path):
+                        os.mkdir(new_folder_path)
 
+                    # Use shutil.move to move the file to the respective folder
+                    new_file_path = os.path.join(new_folder_path, file_name)
+                    shutil.move(file_path, new_file_path)
+                    print(f"Moved {file_name} to {new_folder_path}")
+                    break
+            else:
+                print(f"No appropriate folder found for {file_name}")
