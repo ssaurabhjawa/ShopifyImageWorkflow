92aa048bd97ce5abe6ea07ccbface84f875ac4ea
Saurabh Jawa <ssaurabhjawa@gmail.com>
2023-05-12 16:53:55 +0530

    cloudinaryFolder

--- ShopifyImageWorkflow_v1.py
+++ ShopifyImageWorkflow_v1.py
@@ -381,35 +381,6 @@ update_position_button = tk.Button(root, text="Update Image Position", command=u
 update_position_button.grid(row=3, column=3, padx=5, pady=5)
 
 
-
-
-
-            
-
-
-# #==================================================================
-# #              Step 1. Upload to Cloudinary & URL
-# #==================================================================
-# from cloudinary.uploader import upload
-# from cloudinary.utils import cloudinary_url
-
-# def upload_to_cloudinary(image_path):
-#     cloudinary.config(
-#     cloud_name = "djqvqmqe2",
-#     api_key = "379169473671185",
-#     api_secret = "HFgkfTbvvKlD0TGtXmQDLBFBDys",
-#     secure = True
-# )
-#     response = cloudinary.uploader.upload(image_path, folder="product-images/")
-#     public_id = response["public_id"]
-#     print(f"Uploaded image {public_id} to Cloudinary")
-#     return public_id
-            
-# def get_image_url_from_cloudinary(public_id):
-#     resource = cloudinary.api.resource(public_id)
-#     return resource["url"]
-
-
 #==================================================================
 #      Step 2. Process Directory
 #==================================================================
@@ -419,7 +390,6 @@ from product_level_dict import product_level_dictionary
 from variant_level_dict import variant_level_dictionary
 from extract_file_info import extract_file_info
 from variant_level_dict_3 import variant_level_dictionary_3
-from variant_level_dict_4 import variant_level_dictionary_4
 
 from datetime import datetime
 import os
